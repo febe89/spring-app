7486b475d1ab0a9c94c6315f31f7277b79d12471
febe89 <fullstack9020@gmail.com>
2022-12-28 15:16:06 +0530

    spring-app

---
+++ src/main/java/codewithquad/Fullstackbackend/controller/UserController.java
@@ -0,0 +1,50 @@
+package codewithquad.Fullstackbackend.controller;
+
+import codewithquad.Fullstackbackend.exception.UserNotFoundException;
+import codewithquad.Fullstackbackend.model.User;
+import codewithquad.Fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user->{
+                    user.setName(newUser.getName());
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    user.setImg(newUser.getImg());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id ){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User deleted with id " + id;
+    }
+}
---
+++ src/test/java/codewithquad/Fullstackbackend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package codewithquad.Fullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
